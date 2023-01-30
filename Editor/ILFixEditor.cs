0be06ba9a7b45e2b481df64d9dbfa537975f8d73
muguangyi <muguangyi@hotmail.com>
2023-01-30 17:58:46 +0800

    Add basic files.

---
+++ Editor/ILFixEditor.cs
@@ -0,0 +1,18 @@
+/*
+ * This file is part of IL.Fix project.
+ *
+ * (c) MuGuangyi <muguangyi@hotmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+using UnityEditor;
+
+namespace IL.Fix.Editor
+{
+    public sealed class ILFixEditor : EditorWindow
+    {
+
+    }
+}
---
+++ Runtime/VM.cs
@@ -0,0 +1,15 @@
+/*
+ * This file is part of IL.Fix project.
+ *
+ * (c) MuGuangyi <muguangyi@hotmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace IL.Fix
+{
+    public sealed class VM
+    {
+    }
+}
